d220c4a9af9cbbc2192eae663dc0ec1aa3477af1
Dmitriy Listopad <dimalist2005@gmail.com>
2024-02-07 22:45:32 +0300

    change markup

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,19 +8,18 @@ defineProps({
 </script>
 
 <template>
-  <div class=
-         "bg-white relative border-2 border-black-100
-          rounded-3xl p-6 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
-  >
-    <img :src="imageUrl" alt="car">
-    <p class="my-5">{{ title }}</p>
-    <div class="flex justify-between ">
-      <img class="mt-2 opacity-60 hover:opacity-100 scale-75 hover:scale-100" src="/public/image 7.svg" alt="info">
-      <div v-if="isAdded">
-        <img src="/public/Group 95.svg" alt="plus">
-      </div>
-      <div v-else>
-        <button @click="onClickAdd" class=" mt-2 p-3 border-2 rounded-xl">Оставить Заявку</button>
+  <div class="bg-white border-2 border-gray-200 rounded-3xl cursor-pointer transition hover:-translate-y-2 hover:shadow-xl grid grid-rows-[auto,1fr] overflow-hidden">
+    <img :src="imageUrl" alt="car" class="object-contain w-full h-48 p-3">
+    <div class="grid grid-rows-2 bg-neutral-700 rounded-b-3xl p-5 text-white">
+      <p class="text-xl font-semibold leading-tight">{{ title }}</p>
+      <div class="flex justify-between items-center mt-4">
+        <img class="h-6 opacity-60 hover:opacity-100 transition-opacity duration-300" src="/public/image 7.svg" alt="info">
+        <div v-if="isAdded">
+          <img src="/public/Group 95.svg" alt="Added">
+        </div>
+        <div v-else>
+          <button @click="onClickAdd" class="bg-green-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl transition-colors duration-300">Оставить Заявку</button>
+        </div>
       </div>
     </div>
   </div>
